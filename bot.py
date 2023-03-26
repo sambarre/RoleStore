416c26036b538b2b9ca083bc153e5c5927453313
Sam Barrett <sambarrett23072@gmail.com>
2023-03-26 11:39:14 +0100

    Update from DeadWeight Main

--- bot.py
+++ bot.py
@@ -21,17 +21,30 @@ def run_discord_bot():
     intents.message_content = True
     client = discord.Client(intents=intents)
 
-    #message runs when bot starts
     @client.event
     async def on_ready():
         print(f'{client.user} is now running!')
 
     @client.event
-    async def on_voice_state_update(member, before, after):
-        if before.channel and not after.channel:
-            print(f'{member} has left the vc')
-            await send_message(user_message="953272075555074142",is_private=False,message="test")
+    async def on_message(message):
+        # Make sure bot doesn't get stuck in an infinite loop
+        if message.author == client.user:
+            return
 
-            
+        # Get data about the user
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        # Debug printing
+        print(f"{username} said: '{user_message}' ({channel})")
+
+        # If the user message contains a '?' in front of the text, it becomes a private message
+        if user_message[0] == '?':
+            user_message = user_message[1:]  # [1:] Removes the '?'
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
     # Remember to run your bot with your personal TOKEN
     client.run(TOKEN)
